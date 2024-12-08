e2109a11b8204b94a8e3f098929618aefa5fe7b5
NicolaazDev <johanhenrique2k24@gmail.com>
2024-12-08 19:14:36 -0300

    edit next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  eslint: {
+    // Warning: This allows production builds to succeed even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
