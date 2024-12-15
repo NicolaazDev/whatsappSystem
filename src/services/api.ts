18e14a039baf0919f951e7f96134435f7603bab1
NicolaazDev <johanhenrique2k24@gmail.com>
2024-12-14 22:46:42 -0300

    edit main

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://whatsapp-backend-l6px.onrender.com/api",
+  baseURL: "https://whatsapp-backend-xiqn.onrender.com/api",
   timeout: 100000,
 });
 
